aad9b39666a2b16708f5530417a37847bd4b6e57
jason <jason@br-envision.com>
2019-02-10 13:11:08 -0500

    minor bug fix

--- mstar/mstar2jpeg.py
+++ mstar/mstar2jpeg.py
@@ -13,17 +13,18 @@ import sys
 import glob
 import subprocess
 
-MSTAR2JPEG = os.path.join(os.path.expandvars("$MSTAR2JPEG_PATH"), 'mstar2jpeg')
+MSTAR2JPEG = os.path.join(os.path.expandvars("$MSTAR_JPEG"), 'mstar2jpeg')
 
 def convert_file(in_file, contrast_enhance, compression_quality,
                  dump_header, verbose):
     """Converts MSTAR data file to JPEG"""
-    jpeg_file = in_file + '.jpg' # append JPEG extension
+    jpeg_file = '.'.join((in_file, 'jpg')) # append JPEG extension
     print(in_file, jpeg_file)
     args = [MSTAR2JPEG, '-i', in_file, '-o', jpeg_file, '-q', str(compression_quality)]
     if contrast_enhance: args.append('-e')
-    if verbose: args.append('v')
-    if dump_header: args.append('h')
+    if verbose: args.append('-v')
+    if dump_header: args.append('-h')
+    print(args)
     subprocess.Popen(args)
     return jpeg_file
 
